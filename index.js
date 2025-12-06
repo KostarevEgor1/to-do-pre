7f48a089d7336a2b29fe58ba8e06851e5bbc2855
Egor Kostarev <kostarev0077@gmail.com>
2025-12-06 10:02:58 +0300

    script.js

--- index.js
+++ index.js
@@ -12,24 +12,68 @@ const formElement = document.querySelector(".to-do__form");
 const inputElement = document.querySelector(".to-do__input");
 
 function loadTasks() {
-
+	const savedTasks = localStorage.getItem("tasks");
+	if (savedTasks) {
+		return JSON.parse(savedTasks);
+	}
+	return items;
 }
+
+formElement.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const newItem = createItem(inputElement.value);
+	listElement.prepend(newItem);
+	saveTasks(getTasksFromDOM());
+	inputElement.value = "";
+});
 
 function createItem(item) {
 	const template = document.getElementById("to-do__item-template");
 	const clone = template.content.querySelector(".to-do__item").cloneNode(true);
-  const textElement = clone.querySelector(".to-do__item-text");
-  const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
-  const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
-  const editButton = clone.querySelector(".to-do__item-button_type_edit");
+	const textElement = clone.querySelector(".to-do__item-text");
+	const deleteButton = clone.querySelector(".to-do__item-button_type_delete");
+	const duplicateButton = clone.querySelector(".to-do__item-button_type_duplicate");
+	const editButton = clone.querySelector(".to-do__item-button_type_edit");
 
+	textElement.textContent = item;
+
+	deleteButton.addEventListener("click", () => {
+		clone.remove();
+		items = getTasksFromDOM();
+		saveTasks(items);
+	});
+
+	duplicateButton.addEventListener("click", () => {
+		const itemName = textElement.textContent;
+		const newItem = createItem(itemName);
+		listElement.prepend(newItem);
+		items = getTasksFromDOM();
+		saveTasks(items);
+	});
+
+	editButton.addEventListener("click", () => {
+		textElement.setAttribute("contenteditable", "true");
+    	textElement.focus();
+	});
+
+	textElement.addEventListener("blur", () => {
+		textElement.setAttribute("contenteditable", "false");
+		saveTasks(getTasksFromDOM());
+	});
+
+	return clone;
 }
 
 function getTasksFromDOM() {
-
+	const itemsNamesElements = listElement.querySelectorAll(".to-do__item-text");
+	const tasks = [];
+	itemsNamesElements.forEach(el => tasks.push(el.textContent));
+	return tasks;
 }
 
 function saveTasks(tasks) {
-
+	localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+items = loadTasks();
+items.forEach((item) => listElement.append(createItem(item)));
